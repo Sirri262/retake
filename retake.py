4546e5bdc8c0a9697376f87d78529c5398c7a4c7
Sirri262 <sirri.efe.cataloglu@akademiata.edu.pl>
2026-01-14 12:37:20 +0000

    My comment

---
+++ retake.py
@@ -0,0 +1,21 @@
+process = int(input('first number: '))
+process1 = int(input('second number: '))
+
+process2 = (input('Please choose process (+ , - , / , *)'))
+
+if process2 == "+":
+    print(process + process1)
+elif process2 == "-":
+    print(process - process1)
+elif process2 == "/":
+    print(process / process1)
+elif process2 == "*":
+    print(process * process1) 
+else:
+    print("wrong process")
+
+student = {
+    "name" : "Sirri"
+    "surname" : "cataloglu"
+    "studennumber" : "34854"
+}
